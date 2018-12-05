11b8282a3fa3b2fdbddc6d10b6fa9c17e16e30a6
WenYan1 <754883113@qq.com>
2018-12-05 17:16:22 +0800

    第一次提交

---
+++ src/main/java/Tempus/cn/Test/App.java
@@ -0,0 +1,13 @@
+package Tempus.cn.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
